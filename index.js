b73235ea40a4948b8d25221f7eb646b460dee969
Anietie Brownson <anni.brownson@gmail.com>
2023-11-10 06:36:33 +0100

    feat: Add some commits

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const express = require('express');
+const express = require('express')
+const bodyParser = require('body-parser')
 const cors = require('cors')
+// const diagnosisRoutes = require('./diagnosisRoutes');
+const PORT = process.env.PORT || 5000
+
+
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors())
 
+// Routes
+// app.use('/api', diagnosisRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Hello peeps!');
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
